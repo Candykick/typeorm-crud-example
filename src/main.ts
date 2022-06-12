c9d8adf92380dbbd740befc3fec73e0c14796a4c
HUHS <ourstockdev@gmail.com>
2022-06-12 21:52:41 +0900

    Heroku 빌드를 위해 git에 파일 추가

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Swagger 설정
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('CRUD example')
     .setDescription('Nest.js + TypeORM CRUD example')
@@ -14,6 +15,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3003);
+  await app.listen(process.env.PORT || 3003);
 }
 bootstrap();
